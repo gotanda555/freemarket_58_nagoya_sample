5f60fee3e080c7ae45bc8ae2ccbc14a00871e973
onikunai <greentea5461@gmail.com>
2019-11-24 15:20:45 +0900

    DB_items2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_050356) do
+ActiveRecord::Schema.define(version: 2019_11_24_061854) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_11_24_050356) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "image_id"
   end
 
 end
