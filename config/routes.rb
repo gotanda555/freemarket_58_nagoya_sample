012c54aa70f5a5ecdd8e6c8fc28b9e757985ee58
daissh04 <nortondiss04@ezweb.ne.jp>
2019-12-07 13:47:55 +0900

    7th

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,14 @@ Rails.application.routes.draw do
       get 'done'
     end
   end
+
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4'
+      get 'done'
+    end
+  end
 end
