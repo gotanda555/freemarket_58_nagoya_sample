604981ee9a12d57590e5c1e9898ef0b9b12d19dc
gotanda555 <crok2558@gmail.com>
2019-12-21 09:49:27 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :category
-  belongs_to :brand, optional: true
+  belongs_to :brand
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: true
   has_many :images
