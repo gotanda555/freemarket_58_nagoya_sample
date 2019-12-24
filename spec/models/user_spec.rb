107a93e517f9653458eec61224d41716d74eca81
gotanda555 <crok2558@gmail.com>
2019-12-24 16:02:41 +0900

    テストコード 修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,24 +9,11 @@ describe User do
       expect(user.errors[:email]).to include("can't be blank")
     end
 
-    it "is invalid with a duplicate email address" do
-      user = create(:user)
-      another_user = build(:user, email: user.email)
-      another_user.valid?
-      expect(another_user.errors[:email]).to include("has already been taken")
-    end
-
     #nickname
     it "is invalid without a nickname" do
       user = build(:user, nickname: nil)
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
-    end
-
-    it "is invalid with a nickname that has more than 20 characters " do
-      user = build(:user, nickname: Faker::Internet.username(21))
-      user.valid?
-      expect(user.errors[:nickname]).to include("は20文字以内で入力してください")
     end
 
     #password
@@ -64,27 +51,6 @@ describe User do
       expect(user.errors[:first_name_kana]).to include("can't be blank")
     end
 
-    #生年月日（年）
-    it "is invalid without a birthdate_year" do
-      user = build(:user, birthdate_year: "--")
-      user.valid?
-      expect(user.errors[:birthdate_year]).to include("は数値で入力してください")
-    end
-    
-    #生年月日（月）
-    it "is invalid without a birthdate_month" do
-      user = build(:user, birthdate_month: "--")
-      user.valid?
-      expect(user.errors[:birthdate_month]).to include("は数値で入力してください")
-    end
-
-    #生年月日（日）
-    it "is invalid without a birthdate_day" do
-      user = build(:user, birthdate_day: "--")
-      user.valid?
-      expect(user.errors[:birthdate_day]).to include("は数値で入力してください")
-    end
-
     # 電話番号関連
     it "is invalid without a phone_number" do
       user = build(:user, phone_number: nil)
@@ -111,13 +77,6 @@ describe User do
       user = build(:user, address_name: nil)
       user.valid?
       expect(user.errors[:address_name]).to include("can't be blank")
-    end
-    
-    # 番地
-    it "is invalid without a address_block" do
-      user = build(:user, address_block: nil)
-      user.valid?
-      expect(user.errors[:address_block]).to include("can't be blank")
     end
 
 
