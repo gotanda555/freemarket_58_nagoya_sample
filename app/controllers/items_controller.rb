89c75d38c443b4aaf51d986a11485b1902e672e6
daissh04 <nortondiss04@ezweb.ne.jp>
2019-12-29 15:55:16 +0900

    2nd

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,34 @@ class ItemsController < ApplicationController
   before_action :move_to_login, only: :check
 
   def index
+    @item = Item.new
     @items = Item.all
+  end
+
+  def get_category_children
+    @category_children = Category.find_by(id: "#{params[:id]}", ancestry: nil).children
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:id]}").children
+    @items = Item.all
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  def get_brand
+    # @brand = Brand.new
+    # @brands = Brand.all
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
   
   def detail
@@ -13,20 +40,28 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
-  
+    @item = Item.create(item_params)
+    binding.pry
+    redirect_to root_path
   end
   
   def new
+      @item = Item.new
+      @item.images.build
+      #@item.brand.build
       @category_parent = ["---"]
       Category.where(ancestry: nil).each do |parent|
-        @category_parent << parent.name
+      @category_parent << parent.name
       end
   end
-  
+
   def check
     @items = Item.all
     @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :status, :body,:category_id, :size, :brand_id, :condition,:burden,:region,:sending_days,:price, brand_attributes: [:brandname],images_attributes: [:image]).merge(saler_id: current_user.id, buyer_id: current_user.id)
   end
   
   def move_to_login
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :brand
+  accepts_nested_attributes_for :brand
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: true
   has_many :images
-
+  accepts_nested_attributes_for :images
   validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999}
 end
