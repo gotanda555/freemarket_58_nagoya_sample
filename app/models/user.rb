92d87d084257a5755300aa40c2fe9eab7c3b02a5
gotanda555 <crok2558@gmail.com>
2020-01-09 17:53:23 +0900

    sns認証

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable,omniauth_providers: [:facebook, :google_oauth2]
 
 
   has_one :card
   has_many :items
   has_many :comments
   has_one :evaluation
+  has_many :sns_credentials
 
     #バリデーション
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -28,4 +29,35 @@ class User < ApplicationRecord
     validates :address_name,            presence: true
     validates :address_block,           presence: true
     validates :address_phone_number,    {allow_blank: true, format: { with: VALID_PHONE_REGEX }}
+
+
+    def self.find_oauth(auth)
+      uid = auth.uid
+      provider = auth.provider
+      snscredential = SnsCredential.where(uid: uid, provider: provider).first
+      if snscredential.present?
+        user = User.where(id: snscredential.user_id).first
+      else
+        user = User.where(email: auth.info.email).first
+        if user.present?
+          SnsCredential.create(
+            uid: uid,
+            provider: provider,
+            user_id: user.id
+            )
+        else
+          user = User.create(
+            nickname: auth.info.name,
+            email:    auth.info.email,
+            password: Devise.friendly_token[0, 20]
+            )
+          SnsCredential.create(
+            uid: uid,
+            provider: provider,
+            user_id: user.id
+            )
+        end
+      end
+      return user
+    end
 end
