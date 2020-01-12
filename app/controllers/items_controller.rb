370d7bc3a8c067734a4e985b54b2a6af475f165a
beckha6 <restart.0911@icloud.com>
2020-01-12 15:47:01 +0900

    5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :move_to_login, only: :check
+  require 'payjp'
 
   def index
     @items = Item.all
@@ -18,6 +19,18 @@ class ItemsController < ApplicationController
   def check
     @items = Item.all
     @item = Item.find(params[:id])
+    card = Card.where(user_id: current_user.id).first
+    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      # redirect_to controller: "card", action: "new"
+    else
+      Payjp.api_key = 'sk_test_e310678fdacba84064864c64'
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
   end
 
   def purchase
@@ -25,16 +38,18 @@ class ItemsController < ApplicationController
 
   def pay
     @item = Item.find(params[:id])
+    card = Card.where(user_id: current_user.id).first
     Payjp.api_key = 'sk_test_e310678fdacba84064864c64'
     charge = Payjp::Charge.create(
     amount: @item.price,
-    card: params['payjp-token'],
+    customer: card.customer_id,
     currency: 'jpy'
     )
-    redirect_to action: :done
+   redirect_to action: :done
   end
 
   def done
+    @item = Item.find(params[:id])
   end
   
   def move_to_login
